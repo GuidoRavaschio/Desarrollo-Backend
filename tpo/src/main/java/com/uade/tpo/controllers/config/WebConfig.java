aeea048e390a001ee53a2030ea29590c813ed99a
GuidoRavaschio <guidoravaschio@gmail.com>
2025-06-05 21:56:01 -0300

    Codigo parte 3
    Co-Authored-By: fedeuade <166051236+fedeuade@users.noreply.github.com>

---
+++ tpo/src/main/java/com/uade/tpo/controllers/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.uade.tpo.controllers.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permite todos los endpoints
+                        .allowedOrigins("http://localhost:8081") // Cambia por la URL de tu frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
