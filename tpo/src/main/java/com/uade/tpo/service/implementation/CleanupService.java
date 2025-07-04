e85e933bf5cd34b914fb03ef2f97f0e29de8d660
GuidoRavaschio <guidoravaschio@gmail.com>
2025-07-04 12:17:20 -0300

    gug

--- tpo/src/main/java/com/uade/tpo/service/implementation/CleanupService.java
+++ tpo/src/main/java/com/uade/tpo/service/implementation/CleanupService.java
@@ -2,6 +2,8 @@ package com.uade.tpo.service.implementation;
 
 import java.time.LocalDateTime;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -15,9 +17,12 @@ public class CleanupService {
     @Autowired
     private TemporaryDataRepository repository;
 
+    private static final Logger logger = LoggerFactory.getLogger(CleanupService.class);
+
     @Scheduled(fixedRate = 60000)
     @Transactional
     public void removeExpiredData() {
+        logger.info("[SCHEDULED] Ejecutando cleanup a las {}", LocalDateTime.now());
         repository.deleteByExpiresAtBefore(LocalDateTime.now());
     }
 }
